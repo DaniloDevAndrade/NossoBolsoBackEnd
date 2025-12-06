f8f84a0906c652c9351e4277966b1651ad74cbec
Danilo Andrade <contato@innoveon.com.br>
2025-12-05 21:01:12 -0300

    initial pnpm

--- dist/server.js
+++ dist/server.js
@@ -17,11 +17,16 @@ const goalsRouter_1 = require("./routes/goalsRouter");
 const accountRouter_1 = require("./routes/accountRouter");
 dotenv_1.default.config();
 const app = (0, express_1.default)();
-const allowedOrigins = [
-    "http://localhost:3000",
-];
+const allowedOrigins = (process.env.CORS_ORIGINS?.split(",") ?? ["http://localhost:3000"]).map(origin => origin.trim());
 const corsOptions = {
-    origin: allowedOrigins,
+    origin: (origin, callback) => {
+        if (!origin)
+            return callback(null, true);
+        if (allowedOrigins.includes(origin)) {
+            return callback(null, true);
+        }
+        return callback(new Error(`Origin ${origin} not allowed by CORS`));
+    },
     credentials: true,
 };
 app.use((0, cors_1.default)(corsOptions));
@@ -36,6 +41,7 @@ app.use("/goals", goalsRouter_1.goalsRouter);
 app.use("/account", accountRouter_1.accountRouter);
 app.use(error_handler_1.errorHandlerMiddleware);
 const PORT = Number(process.env.PORT) || 3333;
+console.log("Allowed CORS Origins:", allowedOrigins);
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
